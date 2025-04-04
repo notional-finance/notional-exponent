53320257180dbe25e24f34b7987057ff2fffe522
Jeff Wu <jeff@notional.finance>
2025-04-04 09:55:37 -0700

    feat: adding generic withdraw managers

---
+++ src/withdraws/GenericERC20.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity >=0.8.28;
+
+import {AbstractWithdrawRequestManager} from "./AbstractWithdrawRequestManager.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract GenericERC20WithdrawRequestManager is AbstractWithdrawRequestManager {
+
+    uint256 private currentRequestId;
+    mapping(uint256 => uint256) private s_withdrawRequestTokens;
+
+    constructor(address _owner, address _erc20) AbstractWithdrawRequestManager(_owner, _erc20, _erc20) { }
+
+    function _initiateWithdrawImpl(
+        address /* account */,
+        uint256 tokensToWithdraw,
+        bool /* isForced */,
+        bytes calldata /* data */
+    ) override internal returns (uint256 requestId) {
+        requestId = ++currentRequestId;
+        s_withdrawRequestTokens[requestId] = tokensToWithdraw;
+    }
+
+    function _stakeTokens(address /* depositToken */, uint256 /* amount */, bytes calldata /* data */) internal override {
+        // No-op
+    }
+
+    function _finalizeWithdrawImpl(
+        address /* account */,
+        uint256 requestId
+    ) internal override returns (uint256 tokensClaimed, bool finalized) {
+        tokensClaimed = s_withdrawRequestTokens[requestId];
+        delete s_withdrawRequestTokens[requestId];
+        finalized = true;
+    }
+
+    function canFinalizeWithdrawRequest(uint256 /* requestId */) public pure override returns (bool) {
+        return true;
+    }
+}
---
+++ src/withdraws/GenericERC4626.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity >=0.8.28;
+
+import {AbstractWithdrawRequestManager} from "./AbstractWithdrawRequestManager.sol";
+import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract GenericERC4626WithdrawRequestManager is AbstractWithdrawRequestManager {
+
+    uint256 private currentRequestId;
+    mapping(uint256 => uint256) private s_withdrawRequestShares;
+
+    constructor(address _owner, address _erc4626) AbstractWithdrawRequestManager(_owner, _erc4626, IERC4626(_erc4626).asset()) { }
+
+    function _initiateWithdrawImpl(
+        address /* account */,
+        uint256 sharesToWithdraw,
+        bool /* isForced */,
+        bytes calldata /* data */
+    ) override internal returns (uint256 requestId) {
+        requestId = ++currentRequestId;
+        s_withdrawRequestShares[requestId] = sharesToWithdraw;
+    }
+
+    function _stakeTokens(address depositToken, uint256 amount, bytes calldata /* data */) internal override {
+        require(depositToken == address(withdrawToken), "Invalid deposit token");
+        IERC20(depositToken).approve(address(yieldToken), amount);
+        IERC4626(yieldToken).deposit(amount, address(this));
+    }
+
+    function _finalizeWithdrawImpl(
+        address /* account */,
+        uint256 requestId
+    ) internal override returns (uint256 tokensClaimed, bool finalized) {
+        uint256 sharesToRedeem = s_withdrawRequestShares[requestId];
+        delete s_withdrawRequestShares[requestId];
+        tokensClaimed = IERC4626(yieldToken).redeem(sharesToRedeem, address(this), address(this));
+        finalized = true;
+    }
+
+    function canFinalizeWithdrawRequest(uint256 /* requestId */) public pure override returns (bool) {
+        return true;
+    }
+}
