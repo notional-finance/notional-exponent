c0bb7ab99fdcb4da85af58223fe18ca6a986ef06
Jeff Wu <jeff@notional.finance>
2025-05-13 11:04:01 -0700

    test: create test environment

---
+++ tests/Mocks.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.29;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {AbstractCustomOracle} from "../src/oracles/AbstractCustomOracle.sol";
+import {AbstractYieldStrategy} from "../src/AbstractYieldStrategy.sol";
+
+contract MockWrapperERC20 is ERC20 {
+    ERC20 public token;
+    uint256 public tokenPrecision;
+
+    constructor(ERC20 _token) ERC20("MockWrapperERC20", "MWE") {
+        token = _token;
+        tokenPrecision = 10 ** token.decimals();
+        _mint(msg.sender, 1000000 * 10e18);
+    }
+
+    function deposit(uint256 amount) public {
+        token.transferFrom(msg.sender, address(this), amount);
+        _mint(msg.sender, amount * 1e18 / tokenPrecision);
+    }
+
+    function withdraw(uint256 amount) public {
+        _burn(msg.sender, amount);
+        token.transfer(msg.sender, amount * tokenPrecision / 1e18);
+    }
+}
+
+contract MockOracle is AbstractCustomOracle {
+
+    int256 public price;
+
+    constructor(int256 _price) AbstractCustomOracle("MockOracle", address(0)) { price = _price; }
+
+    function setPrice(int256 _price) public {
+        price = _price;
+    }
+
+    function _calculateBaseToQuote() internal view override returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) {
+        return (0, price, block.timestamp, block.timestamp, 0);
+    }
+}
+
+contract MockYieldStrategy is AbstractYieldStrategy {
+    constructor(
+        address _asset,
+        address _yieldToken,
+        uint256 _feeRate,
+        address _irm,
+        uint256 _lltv
+    ) AbstractYieldStrategy(_asset, _yieldToken, _feeRate, _irm, _lltv, ERC20(_yieldToken).decimals()) { }
+
+    function _mintYieldTokens(uint256 assets, address /* receiver */, bytes memory /* depositData */) internal override {
+        ERC20(asset).approve(address(yieldToken), type(uint256).max);
+        MockWrapperERC20(yieldToken).deposit(assets);
+    }
+
+    function _redeemShares(uint256 sharesToRedeem, address /* sharesOwner */, bytes memory /* redeemData */) internal override returns (bool wasEscrowed) {
+        uint256 yieldTokensBurned = convertSharesToYieldToken(sharesToRedeem);
+        MockWrapperERC20(yieldToken).withdraw(yieldTokensBurned);
+        wasEscrowed = false;
+    }
+}
---
+++ tests/TestEnvironment.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.29;
+
+import "forge-std/src/Test.sol";
+
+import "./Mocks.sol";
+import "../src/proxy/AddressRegistry.sol";
+import "../src/utils/Constants.sol";
+import "../src/AbstractYieldStrategy.sol";
+import "../src/oracles/AbstractCustomOracle.sol";
+import "../src/proxy/TimelockUpgradeableProxy.sol";
+
+abstract contract TestEnvironment is Test {
+    string RPC_URL = vm.envString("RPC_URL");
+    uint256 FORK_BLOCK = vm.envUint("FORK_BLOCK");
+
+    ERC20 public w;
+    MockOracle public o;
+    IYieldStrategy public y;
+    ERC20 public feeToken;
+    ERC20 public asset;
+    uint256 public defaultDeposit;
+    uint256 public defaultBorrow;
+    uint256 public maxEntryValuationSlippage = 0.0010e18;
+    uint256 public maxExitValuationSlippage = 0.0010e18;
+
+    address public owner = address(0x02479BFC7Dce53A02e26fE7baea45a0852CB0909);
+    ERC20 constant USDC = ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    address constant IRM = address(0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC);
+    address public addressRegistry;
+
+    function deployAddressRegistry() public {
+        address deployer = makeAddr("deployer");
+        vm.prank(deployer);
+        addressRegistry = address(new AddressRegistry(owner, owner, owner));
+        assertEq(address(addressRegistry), address(ADDRESS_REGISTRY), "AddressRegistry is incorrect");
+    }
+
+    function setMaxOracleFreshness() internal {
+        vm.prank(owner);
+        TRADING_MODULE.setMaxOracleFreshness(type(uint32).max);
+    }
+
+    function setUp() public {
+        vm.createSelectFork(RPC_URL, FORK_BLOCK);
+        deployAddressRegistry();
+        setMaxOracleFreshness();
+
+        deployYieldStrategy();
+        TimelockUpgradeableProxy proxy = new TimelockUpgradeableProxy(
+            address(y),
+            abi.encodeWithSelector(Initializable.initialize.selector, abi.encode("name", "symbol"))
+        );
+        y = IYieldStrategy(address(proxy));
+
+        asset = ERC20(y.asset());
+        // Set default fee token, this changes for Convex staked tokens
+        if (address(feeToken) == address(0)) feeToken = w;
+
+        vm.prank(owner);
+        TRADING_MODULE.setPriceOracle(address(w), AggregatorV2V3Interface(address(o)));
+
+        // USDC whale
+        vm.startPrank(0x98C23E9d8f34FEFb1B7BD6a91B7FF122F4e16F5c);
+        USDC.transfer(msg.sender, 100_000e6);
+        USDC.transfer(owner, 15_000_000e6);
+        vm.stopPrank();
+
+        // Deal WETH
+        deal(address(WETH), owner, 1_500_000e18);
+        vm.prank(owner);
+        WETH.transfer(msg.sender, 250_000e18);
+
+        setupLendingRouter();
+
+        postDeploySetup();
+    }
+
+    /*** Virtual Test Functions ***/
+    function getRedeemData(
+        address /* user */,
+        uint256 /* shares */
+    ) internal virtual returns (bytes memory redeemData) {
+        return "";
+    }
+
+    function getDepositData(
+        address /* user */,
+        uint256 /* depositAmount */
+    ) internal virtual returns (bytes memory depositData) {
+        return "";
+    }
+
+    function deployYieldStrategy() internal virtual;
+
+    function postDeploySetup() internal virtual { }
+
+    function setupLendingRouter() internal virtual;
+
+}
