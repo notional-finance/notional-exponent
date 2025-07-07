0e115bece229028f29af83c880b37643f210e830
Jeff Wu <jeff@notional.finance>
2025-07-07 11:37:04 -0700

    fix: first test passed

--- subgraph/tests/address-registry.test.ts
+++ subgraph/tests/address-registry.test.ts
@@ -7,10 +7,9 @@ import {
   afterAll
 } from "matchstick-as/assembly/index"
 import { Address } from "@graphprotocol/graph-ts"
-import { ExampleEntity } from "../generated/schema"
-import { AccountPositionCleared } from "../generated/AddressRegistry/AddressRegistry"
-import { handleAccountPositionCleared } from "../src/address-registry"
-import { createAccountPositionClearedEvent } from "./address-registry-utils"
+import { Account } from "../generated/schema"
+import { handleAccountPositionCreated } from "../src/address-registry"
+import { createAccountPositionCreatedEvent } from "./address-registry-utils"
 
 // Tests structure (matchstick-as >=0.5.0)
 // https://thegraph.com/docs/en/subgraphs/developing/creating/unit-testing-framework/#tests-structure
@@ -24,12 +23,12 @@ describe("Describe entity assertions", () => {
     let lendingRouter = Address.fromString(
       "0x0000000000000000000000000000000000000001"
     )
-    let newAccountPositionClearedEvent = createAccountPositionClearedEvent(
+    let newAccountPositionCreatedEvent = createAccountPositionCreatedEvent(
       account,
       vault,
       lendingRouter
     )
-    handleAccountPositionCleared(newAccountPositionClearedEvent)
+    handleAccountPositionCreated(newAccountPositionCreatedEvent)
   })
 
   afterAll(() => {
@@ -39,27 +38,15 @@ describe("Describe entity assertions", () => {
   // For more test scenarios, see:
   // https://thegraph.com/docs/en/subgraphs/developing/creating/unit-testing-framework/#write-a-unit-test
 
-  test("ExampleEntity created and stored", () => {
-    assert.entityCount("ExampleEntity", 1)
+  test("Account created and stored", () => {
+    assert.entityCount("Account", 1)
 
     // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
     assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
-      "account",
-      "0x0000000000000000000000000000000000000001"
-    )
-    assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
-      "vault",
-      "0x0000000000000000000000000000000000000001"
-    )
-    assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
-      "lendingRouter",
-      "0x0000000000000000000000000000000000000001"
+      "Account",
+      "0x0000000000000000000000000000000000000001",
+      "systemAccountType",
+      "None"
     )
 
     // More assert options:
