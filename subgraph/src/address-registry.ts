4a1b4c24e7d5b047f6d5d7dde5267a65f50a8d90
Jeff Wu <jeff@notional.finance>
2025-07-07 11:37:04 -0700

    fix: first test passed

--- subgraph/src/address-registry.ts
+++ subgraph/src/address-registry.ts
@@ -12,91 +12,55 @@ import {
   WhitelistedVault,
   WithdrawRequestManagerSet
 } from "../generated/AddressRegistry/AddressRegistry"
-import { ExampleEntity } from "../generated/schema"
+import { Account } from "../generated/schema"
 
 export function handleAccountPositionCleared(
   event: AccountPositionCleared
-): void {
-  // Entities can be loaded from the store using an ID; this ID
-  // needs to be unique across all entities of the same type
-  const id = event.transaction.hash.concat(
-    Bytes.fromByteArray(Bytes.fromBigInt(event.logIndex))
-  )
-  let entity = ExampleEntity.load(id)
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(id)
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.account = event.params.account
-  entity.vault = event.params.vault
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.feeReceiver(...)
-  // - contract.getVaultPosition(...)
-  // - contract.getWithdrawRequestManager(...)
-  // - contract.isLendingRouter(...)
-  // - contract.lendingRouters(...)
-  // - contract.pauseAdmin(...)
-  // - contract.pendingPauseAdmin(...)
-  // - contract.pendingUpgradeAdmin(...)
-  // - contract.upgradeAdmin(...)
-  // - contract.whitelistedVaults(...)
-  // - contract.withdrawRequestManagers(...)
-}
+): void { }
 
 export function handleAccountPositionCreated(
   event: AccountPositionCreated
-): void {}
+): void {
+  const id = event.params.account.toHexString();
+  let account = Account.load(id);
+  if (!account) {
+    account = new Account(id);
+    account.firstUpdateBlockNumber = event.block.number;
+    account.firstUpdateTimestamp = event.block.timestamp.toI32();
+    account.firstUpdateTransactionHash = event.transaction.hash;
+  }
 
-export function handleFeeReceiverTransferred(
-  event: FeeReceiverTransferred
-): void {}
+  account.lastUpdateBlockNumber = event.block.number;
+  account.lastUpdateTimestamp = event.block.timestamp.toI32();
+  account.lastUpdateTransactionHash = event.transaction.hash;
+  account.systemAccountType = 'None';
+
+  account.save();
+}
 
 export function handleLendingRouterSet(event: LendingRouterSet): void {}
-
-export function handlePauseAdminTransferred(
-  event: PauseAdminTransferred
-): void {}
-
-export function handlePendingPauseAdminSet(event: PendingPauseAdminSet): void {}
-
-export function handlePendingUpgradeAdminSet(
-  event: PendingUpgradeAdminSet
-): void {}
-
-export function handleUpgradeAdminTransferred(
-  event: UpgradeAdminTransferred
-): void {}
 
 export function handleWhitelistedVault(event: WhitelistedVault): void {}
 
 export function handleWithdrawRequestManagerSet(
   event: WithdrawRequestManagerSet
 ): void {}
+
+
+// export function handleFeeReceiverTransferred(
+//   event: FeeReceiverTransferred
+// ): void {}
+
+// export function handlePauseAdminTransferred(
+//   event: PauseAdminTransferred
+// ): void {}
+
+// export function handlePendingPauseAdminSet(event: PendingPauseAdminSet): void {}
+
+// export function handlePendingUpgradeAdminSet(
+//   event: PendingUpgradeAdminSet
+// ): void {}
+
+// export function handleUpgradeAdminTransferred(
+//   event: UpgradeAdminTransferred
+// ): void {}
