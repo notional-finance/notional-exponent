d524ca766cb0c928d2e73e11aa1e9aee52f6c2fb
Jeff Wu <jeff@notional.finance>
2025-07-08 10:06:04 -0700

    fix: whitelist tests passing

---
+++ subgraph/src/constants.ts
@@ -0,0 +1,7 @@
+import { Address } from "@graphprotocol/graph-ts";
+
+export const ZERO_ADDRESS = Address.zero();
+export const UNDERLYING = "Underlying";
+export const VAULT_SHARE = "VaultShare";
+export const VAULT_DEBT = "VaultDebt";
+export const FIAT = "Fiat";
--- subgraph/src/entities/token.ts
+++ subgraph/src/entities/token.ts
@@ -1,12 +1,67 @@
+import { Address, BigInt, ethereum } from "@graphprotocol/graph-ts";
+import { IERC20Metadata } from "../../generated/AddressRegistry/IERC20Metadata";
 import { Token } from "../../generated/schema";
+import { ZERO_ADDRESS } from "../constants";
 
-export function createToken(address: string): string {
-  let token = Token.load(address);
-  if (!token) {
-    token = new Token(address);
+export function getTokenNameAndSymbol(erc20: IERC20Metadata): string[] {
+  let nameResult = erc20.try_name();
+  let name: string;
+  let symbol: string;
+  if (nameResult.reverted) {
+    name = "unknown";
+  } else {
+    name = nameResult.value;
   }
+
+  let symbolResult = erc20.try_symbol();
+  if (symbolResult.reverted) {
+    symbol = "unknown";
+  } else {
+    symbol = symbolResult.value;
+  }
+
+  return [name, symbol];
+}
+
+export function createERC20TokenAsset(
+  tokenAddress: Address,
+  event: ethereum.Event,
+  tokenType: string
+): Token {
+  let token = Token.load(tokenAddress.toHexString());
+  if (token) return token;
+
+  // If token does not exist, then create it here
+  token = new Token(tokenAddress.toHexString());
+
+  if (tokenAddress == ZERO_ADDRESS) {
+    token.name = "Ether";
+    token.symbol = "ETH";
+    token.decimals = 18;
+    token.precision = BigInt.fromI32(10).pow(18);
+  } else {
+    let erc20 = IERC20Metadata.bind(tokenAddress);
+    let symbolAndName = getTokenNameAndSymbol(erc20);
+    let decimals = erc20.decimals();
+    token.name = symbolAndName[0];
+    token.symbol = symbolAndName[1];
+    token.decimals = decimals;
+    token.precision = BigInt.fromI32(10).pow(decimals as u8);
+  }
+
+  token.tokenInterface = "ERC20";
+  token.tokenAddress = tokenAddress;
+  token.tokenType = tokenType;
+
+  token.lastUpdateBlockNumber = event.block.number;
+  token.lastUpdateTimestamp = event.block.timestamp.toI32();
+  token.lastUpdateTransactionHash = event.transaction.hash;
+
+  token.firstUpdateBlockNumber = event.block.number;
+  token.firstUpdateTimestamp = event.block.timestamp.toI32();
+  token.firstUpdateTransactionHash = event.transaction.hash;
 
   token.save();
 
-  return address;
+  return token;
 }
