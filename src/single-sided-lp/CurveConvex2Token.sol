e0a4a173892e65355c050a4398e47265000fdcb8
Jeff Wu <jeff@notional.finance>
2025-09-22 10:35:35 -0700

    fix: check the eth address for curve v2 pools

--- src/single-sided-lp/CurveConvex2Token.sol
+++ src/single-sided-lp/CurveConvex2Token.sol
@@ -276,13 +276,12 @@ contract CurveConvexLib is BaseLPLib {
                 );
             } else {
                 exitBalances[_PRIMARY_INDEX] = ICurve2TokenPoolV2(CURVE_POOL).remove_liquidity_one_coin(
-                    // Last two parameters are useEth and receiver = this contract. In the CurveV2 code,
-                    // the ETH_INDEX is hardcoded to 0 so we check if the first token is ETH and have it return
-                    // ETH in this case so that we can wrap it back into WETH if required.
+                    // Last two parameters are useEth and receiver = this contract. Check if one of the tokens
+                    // is ETH and have it return ETH in this case so that we can wrap it back into WETH if required.
                     poolClaim,
                     _PRIMARY_INDEX,
                     _minAmounts[_PRIMARY_INDEX],
-                    TOKEN_1 == ETH_ADDRESS,
+                    TOKEN_1 == ETH_ADDRESS || TOKEN_2 == ETH_ADDRESS,
                     address(this)
                 );
             }
@@ -308,12 +307,11 @@ contract CurveConvexLib is BaseLPLib {
                 // Remove liquidity on CurveV2 does not return the exit amounts so we have to measure
                 // them before and after.
                 ICurve2TokenPoolV2(CURVE_POOL).remove_liquidity(
-                    // Last two parameters are useEth and receiver = this contract. In the CurveV2 code,
-                    // the ETH_INDEX is hardcoded to 0 so we check if the first token is ETH and have it return
-                    // ETH in this case so that we can wrap it back into WETH if required.
+                    // Last two parameters are useEth and receiver = this contract. Check if one of the tokens
+                    // is ETH and have it return ETH in this case so that we can wrap it back into WETH if required.
                     poolClaim,
                     minAmounts,
-                    TOKEN_1 == ETH_ADDRESS,
+                    TOKEN_1 == ETH_ADDRESS || TOKEN_2 == ETH_ADDRESS,
                     address(this)
                 );
                 exitBalances[0] = TokenUtils.tokenBalance(TOKEN_1) - exitBalances[0];
