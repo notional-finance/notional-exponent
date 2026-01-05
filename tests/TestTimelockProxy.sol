459b259e3838048ffe53e8b0eafe31aefd957153
Jeff Wu <jeff@notional.finance>
2026-01-05 09:18:43 -0800

    test: adding pause contracts tests

--- tests/TestTimelockProxy.sol
+++ tests/TestTimelockProxy.sol
@@ -52,6 +52,14 @@ contract TestTimelockProxy is Test {
         proxy = new TimelockUpgradeableProxy(
             address(impl), abi.encodeWithSelector(Initializable.initialize.selector, abi.encode("name", "symbol"))
         );
+
+        // Upgrade the AddressRegistry
+        address newAddressRegistry = address(new AddressRegistry());
+        vm.startPrank(upgradeOwner);
+        TimelockUpgradeableProxy(payable(address(ADDRESS_REGISTRY))).initiateUpgrade(newAddressRegistry);
+        vm.warp(block.timestamp + 7 days);
+        TimelockUpgradeableProxy(payable(address(ADDRESS_REGISTRY))).executeUpgrade(bytes(""));
+        vm.stopPrank();
     }
 
     function test_cannotReinitializeImplementation() public {
@@ -202,5 +210,211 @@ contract TestTimelockProxy is Test {
         registry.transferFeeReceiver(newFeeReceiver);
 
         assertEq(registry.feeReceiver(), newFeeReceiver);
+    }
+
+    function test_addPausableContract_onlyUpgradeAdmin() public {
+        // Deploy a new TimelockUpgradeableProxy to simulate a new pausable contract
+        Initializable newImpl = new MockInitializable();
+        TimelockUpgradeableProxy newProxy = new TimelockUpgradeableProxy(
+            address(newImpl), abi.encodeWithSelector(Initializable.initialize.selector, abi.encode("name", "symbol"))
+        );
+
+        // Non-upgradeAdmin cannot add pausable contracts
+        vm.expectRevert(abi.encodeWithSelector(Unauthorized.selector, address(this)));
+        registry.addPausableContract(address(newProxy));
+
+        // PauseAdmin cannot add pausable contracts
+        vm.expectRevert(abi.encodeWithSelector(Unauthorized.selector, pauseOwner));
+        vm.prank(pauseOwner);
+        registry.addPausableContract(address(newProxy));
+
+        // UpgradeAdmin can add pausable contracts
+        vm.expectEmit(true, true, true, true);
+        emit AddressRegistry.PausableContractAdded(address(newProxy));
+        vm.prank(upgradeOwner);
+        registry.addPausableContract(address(newProxy));
+
+        // Verify the contract was added
+        address[] memory pausableContracts = registry.getAllPausableContracts();
+        bool found = false;
+        for (uint256 i = 0; i < pausableContracts.length; i++) {
+            if (pausableContracts[i] == address(newProxy)) {
+                found = true;
+                break;
+            }
+        }
+        assertTrue(found, "Pausable contract should be in the list");
+    }
+
+    function test_addPausableContract_multipleContracts() public {
+        // Deploy multiple new proxies
+        Initializable impl1 = new MockInitializable();
+        TimelockUpgradeableProxy proxy1 = new TimelockUpgradeableProxy(
+            address(impl1), abi.encodeWithSelector(Initializable.initialize.selector, abi.encode("name1", "symbol1"))
+        );
+
+        Initializable impl2 = new MockInitializable();
+        TimelockUpgradeableProxy proxy2 = new TimelockUpgradeableProxy(
+            address(impl2), abi.encodeWithSelector(Initializable.initialize.selector, abi.encode("name2", "symbol2"))
+        );
+
+        Initializable impl3 = new MockInitializable();
+        TimelockUpgradeableProxy proxy3 = new TimelockUpgradeableProxy(
+            address(impl3), abi.encodeWithSelector(Initializable.initialize.selector, abi.encode("name3", "symbol3"))
+        );
+
+        uint256 initialLength = registry.getAllPausableContracts().length;
+
+        // Add all three contracts
+        vm.startPrank(upgradeOwner);
+        registry.addPausableContract(address(proxy1));
+        registry.addPausableContract(address(proxy2));
+        registry.addPausableContract(address(proxy3));
+        vm.stopPrank();
+
+        // Verify all contracts were added
+        address[] memory pausableContracts = registry.getAllPausableContracts();
+        assertEq(pausableContracts.length, initialLength + 3, "Should have added 3 contracts");
+
+        // Verify each contract is in the list
+        bool found1 = false;
+        bool found2 = false;
+        bool found3 = false;
+        for (uint256 i = 0; i < pausableContracts.length; i++) {
+            if (pausableContracts[i] == address(proxy1)) found1 = true;
+            if (pausableContracts[i] == address(proxy2)) found2 = true;
+            if (pausableContracts[i] == address(proxy3)) found3 = true;
+        }
+        assertTrue(found1, "Proxy1 should be in the list");
+        assertTrue(found2, "Proxy2 should be in the list");
+        assertTrue(found3, "Proxy3 should be in the list");
+    }
+
+    function test_removePausableContract_onlyUpgradeAdmin() public {
+        // First add a contract
+        Initializable newImpl = new MockInitializable();
+        TimelockUpgradeableProxy newProxy = new TimelockUpgradeableProxy(
+            address(newImpl), abi.encodeWithSelector(Initializable.initialize.selector, abi.encode("name", "symbol"))
+        );
+
+        vm.startPrank(upgradeOwner);
+        registry.addPausableContract(address(newProxy));
+        vm.stopPrank();
+
+        // Get the index of the contract we just added
+        address[] memory contracts = registry.getAllPausableContracts();
+        uint256 index = 0;
+        for (uint256 i = 0; i < contracts.length; i++) {
+            if (contracts[i] == address(newProxy)) {
+                index = i;
+                break;
+            }
+        }
+
+        uint256[] memory indexes = new uint256[](1);
+        indexes[0] = index;
+
+        // Non-upgradeAdmin cannot remove pausable contracts
+        vm.expectRevert(abi.encodeWithSelector(Unauthorized.selector, address(this)));
+        registry.removePausableContract(indexes);
+
+        // PauseAdmin cannot remove pausable contracts
+        vm.expectRevert(abi.encodeWithSelector(Unauthorized.selector, pauseOwner));
+        vm.prank(pauseOwner);
+        registry.removePausableContract(indexes);
+
+        // UpgradeAdmin can remove pausable contracts
+        vm.expectEmit(true, true, true, true);
+        emit AddressRegistry.PausableContractsRemoved(address(newProxy));
+        vm.prank(upgradeOwner);
+        registry.removePausableContract(indexes);
+
+        // Verify the contract was removed
+        address[] memory remainingContracts = registry.getAllPausableContracts();
+        bool found = false;
+        for (uint256 i = 0; i < remainingContracts.length; i++) {
+            if (remainingContracts[i] == address(newProxy)) {
+                found = true;
+                break;
+            }
+        }
+        assertFalse(found, "Pausable contract should not be in the list");
+        assertEq(remainingContracts.length, contracts.length - 1, "Should have removed 1 contract");
+    }
+
+    function test_removePausableContract_multipleContracts() public {
+        // Add multiple contracts
+        Initializable impl1 = new MockInitializable();
+        TimelockUpgradeableProxy proxy1 = new TimelockUpgradeableProxy(
+            address(impl1), abi.encodeWithSelector(Initializable.initialize.selector, abi.encode("name1", "symbol1"))
+        );
+
+        Initializable impl2 = new MockInitializable();
+        TimelockUpgradeableProxy proxy2 = new TimelockUpgradeableProxy(
+            address(impl2), abi.encodeWithSelector(Initializable.initialize.selector, abi.encode("name2", "symbol2"))
+        );
+
+        Initializable impl3 = new MockInitializable();
+        TimelockUpgradeableProxy proxy3 = new TimelockUpgradeableProxy(
+            address(impl3), abi.encodeWithSelector(Initializable.initialize.selector, abi.encode("name3", "symbol3"))
+        );
+
+        vm.startPrank(upgradeOwner);
+        registry.addPausableContract(address(proxy1));
+        registry.addPausableContract(address(proxy2));
+        registry.addPausableContract(address(proxy3));
+        vm.stopPrank();
+
+        // Get indices of contracts to remove
+        address[] memory contracts = registry.getAllPausableContracts();
+        uint256 index1 = 0;
+        uint256 index2 = 0;
+        for (uint256 i = 0; i < contracts.length; i++) {
+            if (contracts[i] == address(proxy1)) index1 = i;
+            if (contracts[i] == address(proxy2)) index2 = i;
+        }
+
+        // Remove two contracts
+        uint256[] memory indexes = new uint256[](2);
+        indexes[0] = index1;
+        indexes[1] = index2;
+
+        vm.startPrank(upgradeOwner);
+        registry.removePausableContract(indexes);
+        vm.stopPrank();
+
+        // Verify contracts were removed
+        address[] memory remainingContracts = registry.getAllPausableContracts();
+        assertEq(remainingContracts.length, contracts.length - 2, "Should have removed 2 contracts");
+
+        // Verify proxy3 is still there
+        bool found3 = false;
+        for (uint256 i = 0; i < remainingContracts.length; i++) {
+            if (remainingContracts[i] == address(proxy3)) {
+                found3 = true;
+                break;
+            }
+        }
+        assertTrue(found3, "Proxy3 should still be in the list");
+    }
+
+    function test_getAllPausableContracts() public {
+        // Get initial contracts
+        address[] memory initialContracts = registry.getAllPausableContracts();
+        uint256 initialLength = initialContracts.length;
+
+        // Add a contract
+        Initializable newImpl = new MockInitializable();
+        TimelockUpgradeableProxy newProxy = new TimelockUpgradeableProxy(
+            address(newImpl), abi.encodeWithSelector(Initializable.initialize.selector, abi.encode("name", "symbol"))
+        );
+
+        vm.prank(upgradeOwner);
+        registry.addPausableContract(address(newProxy));
+
+        // Verify getAllPausableContracts returns the updated list
+        address[] memory allContracts = registry.getAllPausableContracts();
+        assertEq(allContracts.length, initialLength + 1, "Should have one more contract");
+        assertEq(allContracts[allContracts.length - 1], address(newProxy), "Last contract should be the new proxy");
     }
 }
