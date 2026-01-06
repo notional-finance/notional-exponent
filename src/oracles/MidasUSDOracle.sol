86b597c0ecfb9ddcd22c07f8e2e20b69a07f83ef
Jeff Wu <jeff@notional.finance>
2026-01-06 10:35:40 -0800

    fix: switch aggregator source

--- src/oracles/MidasUSDOracle.sol
+++ src/oracles/MidasUSDOracle.sol
@@ -13,13 +13,15 @@ contract MidasUSDOracle is AbstractCustomOracle {
 
     AggregatorV2V3Interface public constant usdcToUSDOracle =
         AggregatorV2V3Interface(0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6);
-    IMidasVault public immutable midasVault;
+    AggregatorV2V3Interface public immutable midasOracle;
     int256 public immutable usdcToUSDDecimals;
+    int256 public immutable midasDecimals;
 
     constructor(string memory description_, IMidasVault midasVault_) AbstractCustomOracle(description_, address(0)) {
         require(block.chainid == CHAIN_ID_MAINNET);
         usdcToUSDDecimals = int256(10 ** usdcToUSDOracle.decimals());
-        midasVault = midasVault_;
+        midasOracle = AggregatorV2V3Interface(IMidasDataFeed(midasVault_.mTokenDataFeed()).aggregator());
+        midasDecimals = int256(10 ** midasOracle.decimals());
     }
 
     function _calculateBaseToQuote()
@@ -28,12 +30,13 @@ contract MidasUSDOracle is AbstractCustomOracle {
         override
         returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
     {
-        int256 mTokenRate = IMidasDataFeed(midasVault.mTokenDataFeed()).getDataInBase18().toInt();
+        int256 mTokenRate;
+        (roundId, mTokenRate, startedAt, updatedAt, answeredInRound) = midasOracle.latestRoundData();
         // Offset the USDC/USD rate to ensure that the USDC/USD rate is hardcoded to 1 in the trading
         // module. In the vaults we request the mToken/USDC rate so we want to offset any USDC variations here.
         int256 usdcToUSD;
-        (roundId, usdcToUSD, startedAt, updatedAt, answeredInRound) = usdcToUSDOracle.latestRoundData();
-        require(usdcToUSD > 0, "Chainlink Rate Error");
-        answer = (mTokenRate * usdcToUSD) / usdcToUSDDecimals;
+        (, usdcToUSD,,,) = usdcToUSDOracle.latestRoundData();
+        require(usdcToUSD > 0 && mTokenRate > 0, "Chainlink Rate Error");
+        answer = (mTokenRate * usdcToUSD * 1e18) / (usdcToUSDDecimals * midasDecimals);
     }
 }
