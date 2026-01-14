9aef98bb312193a0d31178c4fc247d2ed3260425
Jeff Wu <jeff@notional.finance>
2026-01-14 11:36:23 -0800

    fix: update midas oracle timestamp

--- src/oracles/MidasOracle.sol
+++ src/oracles/MidasOracle.sol
@@ -37,16 +37,17 @@ contract MidasOracle is AbstractCustomOracle {
     {
         (AggregatorV2V3Interface baseToUSDOracle, uint8 baseToUSDDecimals) = TRADING_MODULE.priceOracles(baseToken);
         int256 mTokenRate;
-        (roundId, mTokenRate, startedAt, updatedAt, answeredInRound) = midasOracle.latestRoundData();
+        uint256 mTokenUpdatedAt;
+        (roundId, mTokenRate, startedAt, mTokenUpdatedAt, answeredInRound) = midasOracle.latestRoundData();
         // Offset the base/USD rate to ensure that the base/USD rate is hardcoded to 1 in the trading
         // module. In the vaults we request the mToken/base rate so we want to offset any base variations here.
         int256 baseToUSD;
-        uint256 baseUpdatedAt;
-        (, baseToUSD,, baseUpdatedAt,) = baseToUSDOracle.latestRoundData();
+        (, baseToUSD,, updatedAt,) = baseToUSDOracle.latestRoundData();
         require(baseToUSD > 0 && mTokenRate > 0, "Chainlink Rate Error");
 
-        // Use the older timestamp to ensure that the stale price checks works correctly.
-        if (baseUpdatedAt < updatedAt) updatedAt = baseUpdatedAt;
+        // mTokenUpdatedAt is only refreshed once or twice a week so we only return it if it is lagging by more than 7
+        // days. This will cause the stale price check to revert any action.
+        if (mTokenUpdatedAt < updatedAt && (mTokenUpdatedAt < block.timestamp - 7 days)) updatedAt = mTokenUpdatedAt;
         answer = (mTokenRate * baseToUSD * 1e18) / (int256(10 ** baseToUSDDecimals) * midasDecimals);
     }
 }
