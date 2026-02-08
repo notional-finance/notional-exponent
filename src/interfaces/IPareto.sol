a1851e51ca2f55424ed2045ab8f35ba70bdbce0b
Jeff Wu <jeff@notional.finance>
2026-02-08 14:14:13 -0800

    fix: update to use withdraw queue

--- src/interfaces/IPareto.sol
+++ src/interfaces/IPareto.sol
@@ -25,3 +25,18 @@ interface IdleCDOEpochVariant {
     function claimInstantWithdrawRequest() external;
     function claimWithdrawRequest() external;
 }
+
+interface IdleCDOEpochQueue {
+    function tranche() external view returns (address);
+    function idleCDOEpoch() external view returns (address);
+    function epochWithdrawPrice(uint256 epoch) external view returns (uint256);
+    function epochPendingClaims(uint256 epoch) external view returns (uint256);
+
+    function requestDeposit(uint256 amount) external;
+    function requestWithdraw(uint256 amount) external;
+    function userDepositsEpochs(address user, uint256 epoch) external view returns (uint256);
+    function userWithdrawalsEpochs(address user, uint256 epoch) external view returns (uint256);
+
+    function claimDepositRequest(uint256 _epoch) external;
+    function claimWithdrawRequest(uint256 _epoch) external;
+}
